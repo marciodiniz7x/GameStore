2cc708fb2354dc65eaabee58ba6b0935588f59d7
Márcio Diniz <marciodiniz.dev@gmail.com>
2023-09-25 18:42:51 -0300

    Updates

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+// Seletor de elementos
+const main = document.querySelector('.main');
+
 // Iniciar site na página "Home"
 function irHome() {
     window.location = 'home.html';
+}
+
+// Gerar cards dos jogos
+const game = document.createElement('div');
+game.classList.add('game');
+main.appendChild(game);
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+    main.appendChild(game);
 }
