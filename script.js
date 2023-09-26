2a16278e5f5481913876c698bb25ee7f064d8edd
Márcio Diniz <marcio.sccp10@gmail.com>
2023-09-26 12:05:17 -0300

    Updates

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 // Seletor de elementos
 const main = document.querySelector('.main');
+const jogo1 = document.querySelector('.jogo1');
 
 // Iniciar site na página "Home"
 function irHome() {
     window.location = 'home.html';
 }
 
-// Gerar cards dos jogos
+// Gerar elementos de Jogo 1
+const title = document.createElement('div');
+title.classList.add('title');
+jogo1.appendChild(title);
+
+const titleText = document.createElement('div');
+titleText.classList.add('title-text');
+title.appendChild(titleText);
+
+const titleH1 = document.createElement('div');
+titleH1.innerHTML = 'IMPERD';
+
 const numGames = 8
 
 for (let i = 0; i < numGames; i++) {
@@ -15,4 +27,3 @@ for (let i = 0; i < numGames; i++) {
     main.appendChild(game);
     console.log(i);
 }
-
